b41f0b3597a21f92e2542cc4a01443d0c642490e
justinunbe <justinunbe@gmail.com>
2016-07-22 08:37:06 -0600

    started second page

--- home/homeDirective.js
+++ home/homeDirective.js
@@ -6,7 +6,7 @@ return {
     $(document).ready(function(){
       $('.single-item').slick({
       dots: true,
-      // autoplay: true,
+      autoplay: true,
       adaptiveHeight: true
     });
       });
